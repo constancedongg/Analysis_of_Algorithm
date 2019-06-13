d89e952cc7e73fc3f1e1177c6e5d1ce2a90ab529
Zhengyuan Dong <constancedongg@gmail.com>
2019-06-12 23:43:01 -0400

    quicksort

--- quick_sort.py
+++ quick_sort.py
@@ -1,4 +1,38 @@
 
+'''
+Quicksort is a divide and conquer algorithm （recursively break down the problem into sub problems);
+in-place algorithm; 
+worst-case running time is Θ(n2) ; its average-case running time is Θ(n log n)
 
-def quick_sort(arr):
+Main idea:  Pick an input item, call it pivot, and place it in its final location in the sorted array by re-organizing the array (just pick the last item of arr as pivot)
+            so that:
+             all items ≤ pivot are placed before pivot
+             all items > pivot are placed after pivot
 
+'''
+
+# in-place algorithm, nothing to return
+def quick_sort(arr , left , right):
+    if left < right:      
+        split = Partition(arr, left, right)
+        quick_sort(arr , left , split - 1)
+        quick_sort(arr , split + 1 , right)
+ 
+
+
+
+def Partition(arr , left , right):
+    pivot = arr[right]
+    split  = left - 1
+    for i in range(left , right):
+        if arr[i] <= pivot:
+            arr[i] , arr[split + 1] = arr[split +  1] , arr[i]
+            split += 1
+
+    arr[right] , arr[split + 1] = arr[split + 1] , arr[right]
+    split += 1
+    return split
+
+
+arr = [1,3,7,2,6,4,5]
+quick_sort(arr, 0 , len(arr) - 1)
---
+++ trial0612.py
@@ -0,0 +1,4 @@
+
+
+import math
+print(math.sqrt(365 * 2 * math.log(2)))
